d44b2768acc16791c8b6491967cb2ed1f005a910
bhairavpaintingservice-cell <bhairavpaintingservice@gmail.com>
2026-06-17 12:48:52 +0530

    Update domain to bhairavpaintingservice.vercel.app in app/sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://bhairavpainting.vercel.app'
+  const baseUrl = 'https://bhairavpaintingservice.vercel.app'
 
   return [
     {
