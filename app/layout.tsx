73bc0ca02eb51f652557004add3a7db45d2ec518
bhairavpaintingservice-cell <bhairavpaintingservice@gmail.com>
2026-07-26 20:26:41 +0530

    Add Google Tag Manager GTM-K58KMSPL to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,6 +104,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K58KMSPL');`}} />
+        {/* Google Analytics */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-JP7MH6N3NC"></script>
         <script dangerouslySetInnerHTML={{__html: `
           window.dataLayer = window.dataLayer || [];
@@ -113,6 +116,8 @@ export default function RootLayout({
         `}} />
       </head>
       <body className={`${inter.variable} ${playfair.variable} font-sans antialiased`}>
+        {/* Google Tag Manager noscript */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K58KMSPL" height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
         <Header />
         <main className="pt-16">
           {children}
