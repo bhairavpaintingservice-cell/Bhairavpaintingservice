02d7e53fc7f18843f7f1d683da5b708f08c2bb9e
bhairavpaintingservice-cell <bhairavpaintingservice@gmail.com>
2026-09-19 22:00:19 +0530

    Perf: load GTM/GA via next/script afterInteractive (non-blocking) to fix LCP

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import { Header } from '@/components/header'
@@ -107,18 +108,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      </head>
+      <body className={`${inter.variable} ${playfair.variable} font-sans antialiased`}>
         {/* Google Tag Manager */}
-        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K58KMSPL');`}} />
+        <Script id="gtm-loader" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K58KMSPL');`}} />
         {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JP7MH6N3NC"></script>
-        <script dangerouslySetInnerHTML={{__html: `
+        <Script id="ga-loader" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-JP7MH6N3NC" />
+        <Script id="ga-config" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-JP7MH6N3NC');
         `}} />
-      </head>
-      <body className={`${inter.variable} ${playfair.variable} font-sans antialiased`}>
         {/* Google Tag Manager noscript */}
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K58KMSPL" height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
         <Header />
