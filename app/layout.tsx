f8c50fae574ac195bed26f07e5ccb1d588b5e17e
bhairavpaintingservice-cell <bhairavpaintingservice@gmail.com>
2026-09-20 19:20:16 +0530

    Revert: next/script afterInteractive change made metrics worse, reverting to raw script tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next'
-import Script from 'next/script'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 import { Header } from '@/components/header'
@@ -108,18 +107,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      </head>
-      <body className={`${inter.variable} ${playfair.variable} font-sans antialiased`}>
         {/* Google Tag Manager */}
-        <Script id="gtm-loader" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K58KMSPL');`}} />
+        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K58KMSPL');`}} />
         {/* Google Analytics */}
-        <Script id="ga-loader" strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-JP7MH6N3NC" />
-        <Script id="ga-config" strategy="afterInteractive" dangerouslySetInnerHTML={{__html: `
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JP7MH6N3NC"></script>
+        <script dangerouslySetInnerHTML={{__html: `
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-JP7MH6N3NC');
         `}} />
+      </head>
+      <body className={`${inter.variable} ${playfair.variable} font-sans antialiased`}>
         {/* Google Tag Manager noscript */}
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K58KMSPL" height="0" width="0" style={{display:"none", visibility:"hidden"}}></iframe></noscript>
         <Header />
